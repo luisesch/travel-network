f56751313bece7d8fb0473195f9466ec049dfc3f
Luise Schneider <luise.schneider@berlin.de>
2019-01-21 20:34:55 +0100

    added functionality for adding new travel

--- routes/travel-routes.js
+++ routes/travel-routes.js
@@ -22,12 +22,53 @@ const categories = [
   "Pilgrimage"
 ];
 
+//render page with form to add new travel
 travelRoutes.get(
   "/travel/add",
   ensureLogin.ensureLoggedIn(),
   (req, res, next) => {
-    console.log("render travel/add");
     res.render("travel/add", { cities: cities, categories: categories });
+  }
+);
+
+//get data from form, create new travel and add to database
+travelRoutes.post(
+  "/travel/add",
+  uploadCloud.array("photos"), //upload multiple files
+  (req, res, next) => {
+    //get url of each file and push to new array
+    let urlArray = [];
+    req.files.forEach(file => {
+      urlArray.push(file.url);
+      return;
+    });
+
+    const { title, category, start, description } = req.body;
+
+    if (category == null) {
+      res.render("travel/add", {
+        cities: cities,
+        categories: categories,
+        message: "Choose at least one category."
+      });
+      return;
+    }
+
+    const newTravel = new Travel({
+      photos: urlArray,
+      Category: category,
+      title: title,
+      Description: description,
+      Start: start
+    });
+    newTravel
+      .save()
+      .then(travel => {
+        res.redirect("/profile");
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
 );
 
